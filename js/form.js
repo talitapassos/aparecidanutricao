61570d25f7ea47bd4954cab8f2b2dd73ad8992e6
Talita Passos <tata.ogp@gmail.com>
2020-06-21 01:49:40 -0300

    modificando tabela com ajax

--- js/form.js
+++ js/form.js
@@ -3,31 +3,29 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault()
 
     var form = document.querySelector("#form-adiciona");
-
     var paciente = obtemPacienteFormulario(form);
-
-    var pacienteTr = montaTr(paciente);
-    pacienteTr.classList.add("paciente");
 
     var erros = validaPaciente(paciente);
     console.log(erros);
-
     if(erros.length > 0){
         exibeMensagensDeErro(erros);
-        
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);    
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
 
 function exibeMensagensDeErro(erros){
     var ul = document.querySelector("#mensagens-erro");
